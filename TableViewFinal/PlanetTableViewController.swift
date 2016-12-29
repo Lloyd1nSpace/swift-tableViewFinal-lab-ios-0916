a94041d0cd813f823ebbf130eba7c5c719d42288
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-12-29 18:50:21 -0500

    Lab could use some reworking

--- TableViewFinal/PlanetTableViewController.swift
+++ TableViewFinal/PlanetTableViewController.swift
@@ -9,4 +9,48 @@
 import UIKit
 
 class PlanetTableViewController: UITableViewController {
+    
+    let planetCellIdentifier = "PlanetCell"
+    var planets = [Planet]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        generatePlanetInfo()
+    }
+    
+    func generatePlanetInfo() {
+        let earth = Planet(name: "Earth", numberOfMoons: 1, fullOrbit: 365.26, facts: ["Earth is the only planet not named after a god.", "Earth has a powerful magnetic field.", "Earth was once believed to be the center of the universe."])
+        let mars = Planet(name: "Mars", numberOfMoons: 2, fullOrbit: 687.0, facts: ["Mars and Earth have approximately the same landmass.", "Mars is home to the tallest mountain in the solar system.", "Pieces of Mars have fallen to Earth."])
+        
+        planets = [earth, mars]
+        
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return planets.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: planetCellIdentifier, for: indexPath) as! PlanetTableViewCell
+        cell.planetNameLabel.text = planets[indexPath.row].name
+        cell.numberOfMoonsLabel.text = planets[indexPath.row].numberOfMoons > 1 ? "\(planets[indexPath.row].numberOfMoons) moons" : "\(planets[indexPath.row].numberOfMoons) moon"
+        cell.numberOfDaysFullOrbitLabel.text = "\(planets[indexPath.row].fullOrbit) days (full orbit)"
+        cell.numberOfFacts.text = "\(planets[indexPath.row].facts.count) facts"
+        tableView.rowHeight = 148
+
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier != "factSegue" { return }
+        if let dest = segue.destination as? FactTableViewController,
+            let indexPath = tableView.indexPathForSelectedRow {
+            dest.planet = planets[indexPath.row]
+        }
+    }
+    
 }
